8012ef00f031f35a51ec66f15e1ef3e4b5562ce0
marcelosrg <marcelohrs2005@gmail.com>
2025-02-24 22:03:59 -0300

    feat: setup inicial do projeto

---
+++ src/test/java/br/com/desafio/picpay/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.picpay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
